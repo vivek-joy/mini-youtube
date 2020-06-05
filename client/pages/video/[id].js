f3ac64379b79d1bbc05b42cd84120ea9fd2916b9
vivekjoy <vivek.joy@litmus7.com>
2020-06-06 01:41:45 +0530

    completed video page styles

--- client/pages/video/[id].js
+++ client/pages/video/[id].js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactPlayer from "react-player";
-import Link from "next/link";
-import css from "../../sass/pages/video.module.scss";
-import eye from "../../assets/images/eye.png";
-import user from "../../assets/images/user.png";
-import Button from "../../components/shared/Button";
+import React from 'react';
+import ReactPlayer from 'react-player';
+import Link from 'next/link';
+import css from '../../sass/pages/video.module.scss';
+import eye from '../../assets/images/eye.png';
+import user from '../../assets/images/user.png';
+import Button from '../../components/shared/Button';
+import play from '../../assets/images/play.png';
 
 const Video = (props) => {
   const {
@@ -17,11 +18,19 @@ const Video = (props) => {
     channelLeft,
     channelRight,
     published,
+    small,
+    videoListContainer,
+    videoListImage,
+    videoListBody,
+    videoListDesc,
   } = css;
   return (
     <div className={container}>
       <div className={left}>
-        <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+        <ReactPlayer
+          width={700}
+          url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+        />
 
         <div className={videoHeading}>
           <h4>Video Title</h4>
@@ -29,10 +38,10 @@ const Video = (props) => {
             <img
               src={eye}
               style={{
-                width: "14px",
-                height: "14px",
-                marginRight: "4px",
-                marginTop: "4px",
+                width: '14px',
+                height: '14px',
+                marginRight: '4px',
+                marginTop: '4px',
               }}
             />
             4M views
@@ -57,10 +66,50 @@ const Video = (props) => {
             </div>
           </div>
         </div>
+
+        <div className={channelContainer}>
+          <div>
+            <h5>About:</h5>
+            <div className={small} style={{ marginTop: 8 }}>
+              djkznfbdjkzfnjdnf dnfjsdnlfnlsdjfklsdmfklmdklzfmklsdf
+              sdkfmsdklmfklsdmklfm
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className={right}>
         <div className={ad}>AD SPACE</div>
+        <div className={videoListContainer}>
+          <h4>Up Next</h4>
+
+          {[1, 2, 3, 4, 5, 6].map((d) => (
+            <div key={d} className={videoListBody}>
+              <div className={videoListImage}>
+                <img
+                  src="https://images.unsplash.com/photo-1591296795953-9114115d197b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                  alt="videolist avatar"
+                />
+                <div
+                  className={css['featured-img-hover']}
+                  style={{ position: 'absolute', zIndex: 1 }}
+                >
+                  <img
+                    src={play}
+                    style={{ width: '30px', height: '30px' }}
+                    alt="link to play video"
+                  />
+                </div>
+              </div>
+
+              <div className={videoListDesc}>
+                <h5>Nature is cool. Sun is red</h5>
+                <h6>Education</h6>
+                <small>1.8M views 11 Months ago</small>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
